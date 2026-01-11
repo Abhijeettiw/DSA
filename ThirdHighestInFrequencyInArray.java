ce09241d279a47d28b7dda820666336c968e727e
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-01-11 10:54:32 +0530

    Third Highest In Frequency In Array

--- ThirdHighestInFrequencyInArray.java
+++ ThirdHighestInFrequencyInArray.java
@@ -6,13 +6,13 @@ public class ThirdHighestInFrequencyInArray {
     static Integer method(Integer[] array) {
         Integer value = null;
         Map<Integer, Long> collect = Arrays.stream(array)
-                .collect(Collectors.groupingBy(i -> i, Collectors.counting()));
+                .collect(Collectors.groupingBy(i -> i, LinkedHashMap::new, Collectors.counting()));
         Set<Long> values = new HashSet<>(collect.values());
         ArrayList<Long> longs = new ArrayList<>(values);
         Collections.sort(longs);
         Long targetValue = values.size() > 2 ? longs.get(2) : longs.getLast();
         for (Map.Entry<Integer, Long> entry : collect.entrySet()) {
-            if (targetValue == entry.getValue()) {
+            if (Objects.equals(targetValue, entry.getValue())) {
                 value = entry.getKey();
                 break;
             }
