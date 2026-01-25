246e6c275d07dcb988c1a561c03a11446078c11c
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-01-25 19:51:59 +0530

    Max sub array

--- MaxSubArray.java
+++ MaxSubArray.java
@@ -1,29 +1,22 @@
 public class MaxSubArray {
     public static void main(String[] args) {
-//        int[] array = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        int[] array = {1};
-        int sum = 0, start = -1, end = -1;
-        for (int i = 0; i < array.length; i++) {
-            int initialSum = 0;
-            initialSum = initialSum + array[i];
-            if (array.length > 1) {
-                for (int j = i + 1; j < array.length; j++) {
-                    if (i != j) {
-                        initialSum = initialSum + array[j];
-                        if (sum < initialSum) {
-                            sum = initialSum;
-                            start = i;
-                            end = j;
-                        }
-                    }
-                }
-            } else {
-                sum = initialSum;
-                start = 1;
-                end = 1;
+//        Kaden's Algo
+//        if current sum < 0 reset it to zero
+
+//        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+//        int[] nums = {1};
+        int[] nums = {-2,1};
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+        for(int i =1;i<nums.length;i++){
+            if(currentSum<0){
+                currentSum = 0;
+            }
+            currentSum = currentSum+nums[i];
+            if(maxSum<currentSum){
+                maxSum = currentSum;
             }
         }
-        System.out.println(sum);
-        System.out.println(start + "(Start) - " + end + "(End)");
+        System.out.println(maxSum);
     }
 }
