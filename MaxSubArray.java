5183323f1d8c5a31f22207c33ea04eed13dbfafd
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-01-25 19:55:20 +0530

    Max sub array

--- MaxSubArray.java
+++ MaxSubArray.java
@@ -7,16 +7,20 @@ public class MaxSubArray {
 //        int[] nums = {1};
         int[] nums = {-2,1};
         int maxSum = nums[0];
+        int start=0,end=0;
         int currentSum = nums[0];
         for(int i =1;i<nums.length;i++){
             if(currentSum<0){
                 currentSum = 0;
+                start = i;
             }
             currentSum = currentSum+nums[i];
             if(maxSum<currentSum){
                 maxSum = currentSum;
+                end = i;
             }
         }
         System.out.println(maxSum);
+        System.out.println(start+" - "+end);
     }
 }
