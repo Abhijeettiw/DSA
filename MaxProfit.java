fc791225641755e94828773a0e9b020c7647fcb9
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-01-26 14:24:58 +0530

    Max profit

---
+++ MaxProfit.java
@@ -0,0 +1,24 @@
+public class MaxProfit {
+    public static void main(String[] args) {
+//        int[] array = {7, 1, 5, 3, 6, 4};
+//        int[] array = {7, 6, 5, 3, 5, 4};
+        int[] array = {7, 6, 5, 4, 3, 2};
+        int maxProfit = 0;
+        int currentProfit = 0;
+        int purchaseDay = 0;
+        int sellDay = 0;
+        for (int i = 1; i < array.length; i++) {
+            currentProfit = array[i] - array[purchaseDay];
+            if (maxProfit < currentProfit) {
+                maxProfit = currentProfit;
+                sellDay = i;
+            }
+            if (currentProfit < 0) {
+                currentProfit = array[i];
+                purchaseDay = i;
+            }
+        }
+        System.out.println(maxProfit);
+        System.out.println(purchaseDay + "(P) - " + sellDay + "(S)");
+    }
+}
