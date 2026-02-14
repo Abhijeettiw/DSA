236698ef83811e5ee4290125fce9f7c7a304c782
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-02-14 23:32:50 +0530

    Merge sort

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -3,13 +3,20 @@ package Sorting;
 /*
     Theory
         - Divide array into 2 parts.
-        - Sort both array.
-        - Merge the arrays in to one.
+        - Keep on dividing array until size of each array is 1.
+            eg: {2,1,4,2,5,3,6}
+                {2}{1}{4}{2}{5}{3}{6} - end result of dividing or array
+        - Merge the arrays in to one by merge algorithm.
+            eg: {2}{1}{4}{2}{5}{3}{6}
+                {1,2}{2,4}{3,5},{6}
+                {1,2,2,4},{3,5,6}
+                {1,2,2,3,4,5,6}
 */
 
 import java.util.Arrays;
 
 public class MergeSort {
+    //    for merging sorted array
     static int[] merge(int[] a1, int[] a2) {
         int n1 = 0;
         int n2 = 0;
@@ -36,36 +43,29 @@ public class MergeSort {
         return res;
     }
 
-    static void insertionSort(int[] arr) {
-        for (int i = 1; i < arr.length; i++) {
-            int key = arr[i];
-            int j = i - 1;
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j--;
+    static int[] mergeRecursion(int[] arr) {
+        int[] res = arr;
+        if (arr.length > 1) {
+            int l = arr.length / 2;
+            int[] a1 = new int[l];
+            int[] a2 = new int[arr.length - l];
+            for (int i = 0; i < arr.length; i++) {
+                if (i < l) {
+                    a1[i] = arr[i];
+                } else {
+                    a2[i - l] = arr[i];
+                }
             }
-            arr[j + 1] = key;
+            int[] a3 = mergeRecursion(a1);
+            int[] a4 = mergeRecursion(a2);
+            res = merge(a3, a4);
         }
+        return res;
     }
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 4, 6, 3, 1, 2, 4, 5, 11, 4, 6, 13, 43, 23, 7};
-//        diving into 2 array
-        int l = arr.length / 2;
-        int[] a1 = new int[l];
-        int[] a2 = new int[arr.length - l];
-        System.out.println(Arrays.toString(arr));
-        for (int i = 0; i < arr.length; i++) {
-            if (i < l) {
-                a1[i] = arr[i];
-            } else {
-                a2[i - l] = arr[i];
-            }
-        }
-        insertionSort(a1);
-        insertionSort(a2);
-
-        int[] merge = merge(a1, a2);
-        System.out.println(Arrays.toString(merge));
+        int[] arr = {2,1,4,2,5,3,6};
+//        int[] arr = {1, 2, 4, 6, 3, 1, 2, 4, 5, 11, 4, 6, 13, 43, 23, 7};
+        System.out.println(Arrays.toString(mergeRecursion(arr)));
     }
 }
