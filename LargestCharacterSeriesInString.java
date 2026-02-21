699cb345bfe633d86faa89a9fbb787bd5b72ce34
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-02-21 19:09:07 +0530

    Largest Character Series In String

--- LargestCharacterSeriesInString.java
+++ LargestCharacterSeriesInString.java
@@ -1,17 +1,19 @@
 public class LargestCharacterSeriesInString {
     public static void main(String[] args) {
-        String str = "abcabcdedef";
+        String str = "abcabcdedefg";
         StringBuilder largestSubString = new StringBuilder();
         StringBuilder currentSubString = new StringBuilder();
         int asci = 0;
         for (int i = 0; i < str.length(); i++) {
+
             if (asci < (int) str.charAt(i)) {
                 asci = (int) str.charAt(i);
                 currentSubString.append(str.charAt(i));
-            } else {
-                if(largestSubString.length()<currentSubString.length()){
-                    largestSubString = new StringBuilder(currentSubString);
-                }
+            }
+            if (largestSubString.length() < currentSubString.length()) {
+                largestSubString = new StringBuilder(currentSubString);
+            }
+            if (asci > (int) str.charAt(i)) {
                 asci = 0;
                 currentSubString.setLength(0);
                 currentSubString.append(str.charAt(i));
