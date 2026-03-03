27311dfbeb0f850a17f151f729270cb09b10bb17
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-03-03 15:33:20 +0530

    Middle Of LinkedList

---
+++ LinkedList/MiddleOfLinkedList.java
@@ -0,0 +1,25 @@
+package LinkedList;
+
+public class MiddleOfLinkedList {
+    public static void main(String[] args) {
+        ListNode<Integer> l1 = new ListNode<>(10);
+        l1.addNode(20);
+        l1.addNode(30);
+        l1.addNode(40);
+        l1.addNode(50);
+//        l1.addNode(60);
+        int size = l1.size();
+        int i = 0;
+        int mid = size % 2 == 0 ? size / 2 : size / 2 + 1;
+        ListNode<Integer> midNode = null;
+        ListNode<Integer> cur = (ListNode<Integer>) l1.head;
+        while (cur != null && i < mid) {
+            if (i + 1 == mid) {
+                midNode = cur;
+            }
+            i++;
+            cur = (ListNode<Integer>) cur.next;
+        }
+        System.out.println(midNode.data);
+    }
+}
