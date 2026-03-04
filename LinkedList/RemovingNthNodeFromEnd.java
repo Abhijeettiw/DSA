ee64f727c02af67d283af787e17a85e93387e0c9
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-03-04 13:05:39 +0530

    Removing Nth Node From End

--- LinkedList/RemovingNthNodeFromEnd.java
+++ LinkedList/RemovingNthNodeFromEnd.java
@@ -1,6 +1,33 @@
 package LinkedList;
 
 public class RemovingNthNodeFromEnd {
+    static ListNode<Integer> removeNth(ListNode<Integer> list, int n) {
+        /*
+            Theory (slow & fast pointer)
+            - Initially slow & fast both pointer will bw at start.
+            - Fast will be moved n+1 times at start.
+            - Both fast & slow will move one step until fast reaches to null
+        */
+        if(n>0) {
+            n = n + 1;
+            ListNode<Integer> slow = (ListNode<Integer>) list.head;
+            ListNode<Integer> fast = (ListNode<Integer>) list.head;
+            while (n > 0 && fast != null) {
+                fast = (ListNode<Integer>) fast.next;
+                n--;
+            }
+            while (fast != null) {
+                slow = (ListNode<Integer>) slow.next;
+                fast = (ListNode<Integer>) fast.next;
+            }
+            ListNode<Integer> temp = (ListNode<Integer>) slow.next;
+            slow.next = slow.next.next;
+            temp.next = null;
+            return temp;
+        }
+        return null;
+    }
+
     static ListNode<Integer> removeNthFromEnd(ListNode<Integer> list, int n) {
         if (n >= list.size() - 1 || n <= 0) {
             n = list.size() - 1;
@@ -30,8 +57,10 @@ public class RemovingNthNodeFromEnd {
         l1.addNode(50);
         l1.addNode(60);
 
-        ListNode<Integer> removedList = removeNthFromEnd(l1, -9);
-        System.out.println(removedList != null ? removedList.data : null);
+//        ListNode<Integer> removedList = removeNthFromEnd(l1, -9);
+//        System.out.println(removedList != null ? removedList.data : null);
+        ListNode<Integer> removedList1 = removeNth(l1, 1);
+        System.out.println(removedList1 != null ? removedList1.data : null);
         System.out.println("---------");
         l1.printNode();
     }
