9e6ee83f94498e69e892932d3a82b78f0d097fc2
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-03-14 14:50:05 +0530

    Contains Duplicate

--- HashTable/ContainsDuplicate.java
+++ HashTable/ContainsDuplicate.java
@@ -4,7 +4,7 @@ import java.util.Hashtable;
 
 public class ContainsDuplicate {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,5,3,1,3};
+        int[] arr = {1,2,3,5,0,6,9};
         Hashtable<Integer,Integer> count= new Hashtable<>();
         boolean duplicate = false;
         for (int i : arr){
