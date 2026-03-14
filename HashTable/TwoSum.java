6c9368118c792edfffb88b676361627b40ed6e19
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-03-14 15:12:05 +0530

    Tow sum equal to target

--- HashTable/TwoSum.java
+++ HashTable/TwoSum.java
@@ -5,9 +5,10 @@ import java.util.Hashtable;
 
 public class TwoSum {
     public static void main(String[] args) {
-        int[] arr = {4, 1, 2, 5, 3, 7, 6, 9};
-        int target = 10;
-        int[] res = new int[2];
+        int[] arr = {2,7,11,15};
+//        int[] arr = {4, 1, 2, 5, 3, 7, 6, 9};
+        int target = 9;
+        int[] res = null;
         Hashtable<Integer, Integer> hash = new Hashtable<>();
         for (int i = 0; i < arr.length; i++) {
             hash.put(arr[i],i);
