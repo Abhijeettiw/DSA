4755621c033fb4c970637b8e2f9a45fa1d671273
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-03-15 21:57:37 +0530

    Three sum

--- HashTable/ThreeSum.java
+++ HashTable/ThreeSum.java
@@ -31,12 +31,37 @@ public class ThreeSum {
         return new HashSet<>(result.values());
     }
 
+    static Set<int[]> solution2(int[] arr, int target) {
+        Arrays.sort(arr);
+        Set<int[]> result = new HashSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            int ini = arr[i];
+            int s = i + 1;
+            int e = arr.length - 1;
+            while (s < e) {
+                if (target > arr[s] + arr[e] + ini) {
+                    s++;
+                } else if (target < arr[s] + arr[e] + ini) {
+                    e--;
+                } else {
+                    result.add(new int[]{ini, arr[s], arr[e]});
+                    s++;
+                    e--;
+                }
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
 //        int[] arr = {0, 1, 1};
         int[] arr = {-1, 0, 1, 2, -1, 4};
         int target = 0;
 
         for (int[] a : solution1(arr, target)) {
+            System.out.println(Arrays.toString(a));
+        }
+        for (int[] a : solution2(arr, target)) {
             System.out.println(Arrays.toString(a));
         }
     }
