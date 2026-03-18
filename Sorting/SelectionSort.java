1c64943ec686e91a2eb619e1008ceb7a43cddb35
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-03-18 10:53:25 +0530

    Selection sort optimisation

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -22,13 +22,17 @@ public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = {15, 20, 5, 45, 30, 10};
         for (int i = 0; i < arr.length; i++) {
+            int min = i;
+            inner:
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[i]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+                if (arr[j] < arr[min]) {
+                    min = j;
+                    break inner;
                 }
             }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
         }
         System.out.println(Arrays.toString(arr));
     }
