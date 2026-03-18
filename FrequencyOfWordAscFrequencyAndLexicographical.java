ec985d80c12b71829a9d51a78d7c31f1b6567b43
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-03-18 10:58:29 +0530

    Frequency Of Word Asc Frequency And Lexicographical optimisation

--- FrequencyOfWordAscFrequencyAndLexicographical.java
+++ FrequencyOfWordAscFrequencyAndLexicographical.java
@@ -4,7 +4,7 @@ import java.util.stream.Collectors;
 
 public class FrequencyOfWordAscFrequencyAndLexicographical {
 
-    static Map<String, Integer> basicMethod(String string) {
+    static Map<String, Integer> solution1(String string) {
         Map<String, Integer> count = new HashMap<>();
         for (String i : string.split("")) {
             count.computeIfPresent(i, (k, v) -> v + 1);
@@ -17,17 +17,20 @@ public class FrequencyOfWordAscFrequencyAndLexicographical {
         return res;
     }
 
-    public static void main(String[] args) {
-        String str = "worfbwcwohjgwojwpkwpfracnvbd";
-        Map<String, Long> collect = Arrays.stream(str.split("")).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+    static Map<String, Long> solution2(String string) {
+        Map<String, Long> collect = Arrays.stream(string.split("")).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet().stream().sorted(
                         Map.Entry.<String, Long>comparingByValue()
                                 .thenComparing(Map.Entry.comparingByKey()))
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (a, b) -> a, LinkedHashMap::new));
+        return collect;
+    }
 
-        collect.forEach((k, v) -> System.out.println(k + " - " + v));
+    public static void main(String[] args) {
+        String str = "worfbwcwohjgwojwpkwpfracnvbd";
+        solution2(str).forEach((k, v) -> System.out.println(k + " - " + v));
         System.out.println("----------");
-        Map<String, Integer> stringIntegerMap = basicMethod(str);
+        Map<String, Integer> stringIntegerMap = solution1(str);
         stringIntegerMap.forEach((k, v) -> System.out.println(k + " - " + v));
 
     }
