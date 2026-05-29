8b1f5bc9d637e85288376e4f2b3cc84e38d53d8a
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-05-29 17:19:00 +0530

    Search In Rotated Sorted Array

--- BinarySearch/BinarySearch.java
+++ BinarySearch/BinarySearch.java
@@ -3,7 +3,7 @@ package BinarySearch;
 import java.util.Arrays;
 
 public class BinarySearch {
-    static Integer binarySearch(Integer[] arr, int target) {
+    static int binarySearch(int[] arr, int target) {
         int start = 0;
         int end = arr.length - 1;
         while (start <= end) {
@@ -20,7 +20,7 @@ public class BinarySearch {
     }
 
     public static void main(String[] args) {
-        Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         Arrays.sort(arr);
         System.out.println(binarySearch(arr, 1));
     }
