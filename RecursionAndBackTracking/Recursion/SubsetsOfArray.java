88010df613026c4624286288d2dd5104aea939ad
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-06-01 20:52:11 +0530

    Subset Recursion

---
+++ RecursionAndBackTracking/Recursion/SubsetsOfArray.java
@@ -0,0 +1,33 @@
+package RecursionAndBackTracking.Recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubsetsOfArray {
+    static void subsetRecursion(int[] arr, int idx, List<Integer> cur, List<List<Integer>> res) {
+        if (idx == arr.length) {
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+        cur.add(arr[idx]);
+        subsetRecursion(arr, idx + 1, cur, res);
+        cur.removeLast();
+        subsetRecursion(arr, idx + 1, cur, res);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        List<List<Integer>> result = new ArrayList<>();
+        subsetRecursion(arr, 0, new ArrayList<>(), result);
+        int cnt = 0;
+        for (List<Integer> il : result) {
+            StringBuilder r = new StringBuilder();
+            for (Integer i : il) {
+                r.append(i).append(" ");
+            }
+            System.out.println(r);
+            cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
