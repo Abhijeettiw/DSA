3051d9edf5341e1e26d1a309ceb2b391bdf14909
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-06-13 13:29:52 +0530

    Permutation Of Numbers

---
+++ RecursionAndBackTracking/BackTracking/PermutationOfNumbers.java
@@ -0,0 +1,40 @@
+package RecursionAndBackTracking.BackTracking;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PermutationOfNumbers {
+    static void permutation(int[] nums, int idx, ArrayList<Integer> cur, List<ArrayList<Integer>> res, Map<Integer, Boolean> used) {
+        if (idx == nums.length) {
+            res.add(new ArrayList<>(cur));
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                if (!used.containsKey(nums[i]) || used.get(nums[i]) == false) {
+                    cur.add(nums[i]);
+                    used.put(nums[i], true);
+                    permutation(nums, idx + 1, cur, res, used);
+                    cur.removeLast();
+                    used.put(nums[i], false);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        List<ArrayList<Integer>> result = new ArrayList<>();
+        permutation(nums, 0, new ArrayList<>(), result, new HashMap<>());
+        int cnt = 0;
+        for (List<Integer> il : result) {
+            StringBuilder r = new StringBuilder();
+            for (Integer i : il) {
+                r.append(i).append(" ");
+            }
+            System.out.println(r);
+            cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
--- RecursionAndBackTracking/BackTracking/SubsetsOfArray.java
+++ RecursionAndBackTracking/BackTracking/SubsetsOfArray.java
@@ -1,4 +1,4 @@
-package RecursionAndBackTracking.Recursion;
+package RecursionAndBackTracking.BackTracking;
 
 import java.util.ArrayList;
 import java.util.List;
