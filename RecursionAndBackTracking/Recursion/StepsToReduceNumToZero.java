b00d34643021ad71e8a6bd6c27df7a696818b9f1
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-06-20 16:36:29 +0530

    Recursion Steps To Reduce Num To Zero

--- RecursionAndBackTracking/Recursion/StepsToReduceNumToZero.java
+++ RecursionAndBackTracking/Recursion/StepsToReduceNumToZero.java
@@ -1,21 +1,36 @@
 package RecursionAndBackTracking.Recursion;
 
 public class StepsToReduceNumToZero {
-    static int makeNumZero(int num) {
-        return makeNumZero(num, 0);
+    static int makeNumZeroRecursion(int num) {
+        return makeNumZeroRecursion(num, 0);
     }
 
-    static int makeNumZero(int num, int steps) {
+    static int makeNumZeroRecursion(int num, int steps) {
         if (num == 0) {
             return steps;
         }
         if (num % 2 == 0) {
-            return makeNumZero(num / 2, ++steps);
+            return makeNumZeroRecursion(num / 2, ++steps);
         }
-        return makeNumZero(num - 1, ++steps);
+        return makeNumZeroRecursion(num - 1, ++steps);
+    }
+
+    static int makeNumZero(int num) {
+        int steps = 0;
+        int newNum = num;
+        while (newNum > 0) {
+            if (newNum % 2 == 0) {
+                newNum /= 2;
+            } else {
+                newNum -= 1;
+            }
+            steps++;
+        }
+        return steps;
     }
 
     public static void main(String[] args) {
+        System.out.println(makeNumZeroRecursion(14));
         System.out.println(makeNumZero(14));
     }
 }
