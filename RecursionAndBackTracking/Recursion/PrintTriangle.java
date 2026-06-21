0eae5d92f93c2a75133a53d22debb470e9330e98
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-06-21 21:02:05 +0530

    Recursion Print Triangle * ** *** ****

--- RecursionAndBackTracking/Recursion/PrintTriangle.java
+++ RecursionAndBackTracking/Recursion/PrintTriangle.java
@@ -1,25 +1,42 @@
 package RecursionAndBackTracking.Recursion;
 
 public class PrintTriangle {
-    static void printTriangle(int row) {
-        printTriangle(row, 0);
+    static void printTriangleReverse(int row) {
+        printTriangleReverse(row, 0);
     }
 
-    static void printTriangle(int row, int col) {
+    static void printTriangleReverse(int row, int col) {
         if (row == 0) {
             return;
         }
         if (col < row) {
             System.out.print("*");
-            printTriangle(row, col + 1);
+            printTriangleReverse(row, col + 1);
         } else if (col == row) {
             System.out.println("");
-            printTriangle(row - 1, 0);
+            printTriangleReverse(row - 1, 0);
         }
+    }
 
+    public static void printTriangle(int lines) {
+        printTriangle(lines, 0, 0);
+    }
+
+    public static void printTriangle(int lines, int row, int col) {
+        if (lines == row) {
+            return;
+        }
+        if (col <= row) {
+            System.out.print("*");
+            printTriangle(lines, row, col + 1);
+        } else {
+            System.out.println();
+            printTriangle(lines, row + 1, 0);
+        }
     }
 
     public static void main(String[] args) {
+        printTriangleReverse(4);
         printTriangle(4);
     }
 }
