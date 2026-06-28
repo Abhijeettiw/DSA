3f9cf5f182b4672627eb9e9f041c5f41950bbfea
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-06-28 14:56:20 +0530

    Recursion Mobile Digit Letter Combination

---
+++ RecursionAndBackTracking/Recursion/MobileDigitLetterCombination.java
@@ -0,0 +1,29 @@
+package RecursionAndBackTracking.Recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MobileDigitLetterCombination {
+    static List<String> mobileDigitLetterCombination(String str, String[] valueArr) {
+        return mobileDigitLetterCombination(str, 0, "", valueArr);
+    }
+
+    static List<String> mobileDigitLetterCombination(String str, int index, String currentStr, String[] valueArr) {
+        List<String> result = new ArrayList<>();
+        if (index == str.length()) {
+            result.add(currentStr);
+            return result;
+        }
+        String value = valueArr[str.charAt(index) - '0'];
+        for (int i = 0; i < value.length(); i++) {
+            result.addAll(mobileDigitLetterCombination(str, index + 1, currentStr + value.charAt(i), valueArr));
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] stringArr = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        List<String> strings = mobileDigitLetterCombination("23", stringArr);
+        strings.forEach(System.out::println);
+    }
+}
