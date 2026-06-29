a303e34fb8b34a70ca191a29de1eba8004e8354f
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-06-29 20:51:13 +0530

    Recursion Dice Throw Sum Equals Target

---
+++ RecursionAndBackTracking/Recursion/DiceThrowSumEqualsTarget.java
@@ -0,0 +1,27 @@
+package RecursionAndBackTracking.Recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DiceThrowSumEqualsTarget {
+    static List<String> diceThrowSumEqualsTarget( int target) {
+        return diceThrowSumEqualsTarget("", target);
+    }
+
+    static List<String> diceThrowSumEqualsTarget(String processed, int target) {
+        List<String> result = new ArrayList<>();
+        if (0 == target) {
+            result.add(processed);
+            return result;
+        }
+        for (int i = 1; i <= 6 && i <= target; i++) {
+            result.addAll(diceThrowSumEqualsTarget(processed + i, target - i));
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<String> lists = diceThrowSumEqualsTarget( 4);
+        System.out.println(lists);
+    }
+}
