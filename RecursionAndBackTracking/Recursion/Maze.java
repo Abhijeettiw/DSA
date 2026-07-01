0570774d27abaeac89f428d02a73a8d99fef2b7a
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-07-01 19:05:00 +0530

    Recursion Maze problem

--- RecursionAndBackTracking/Recursion/Maze.java
+++ RecursionAndBackTracking/Recursion/Maze.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Maze {
-    static List<String> ways(int[][] maze, int startRow, int startCol, int endRow, int endCol) {
-        return ways(maze, startRow, startCol, endRow, endCol, "");
+    static List<String> ways(int[][] maze, int startRow, int startCol, int endRow, int endCol, int[][] obstracles) {
+        return ways(maze, startRow, startCol, endRow, endCol, "", obstracles);
     }
 
-    static List<String> ways(int[][] maze, int startRow, int startCol, int endRow, int endCol, String currentWay) {
+    static List<String> ways(int[][] maze, int startRow, int startCol, int endRow, int endCol, String currentWay, int[][] obstracles) {
         List<String> result = new ArrayList<>();
         if (startRow == maze.length || startCol == maze[0].length) {
             return result;
@@ -19,20 +19,48 @@ public class Maze {
         }
 //        Diagonal
         if (startRow < maze.length && startRow < maze.length) {
-            result.addAll(ways(maze, startRow + 1, startCol + 1, endRow, endCol, currentWay + "C"));
+            boolean allowed = true;
+            for (int[] obs : obstracles) {
+                if (startRow + 1 == obs[0] && startCol + 1 == obs[1]) {
+                    allowed = false;
+                    break;
+                }
+            }
+            if (allowed) {
+                result.addAll(ways(maze, startRow + 1, startCol + 1, endRow, endCol, currentWay + "C", obstracles));
+            }
         }
 //        Down
         if (startRow < maze.length) {
-            result.addAll(ways(maze, startRow + 1, startCol, endRow, endCol, currentWay + "D"));
+            boolean allowed = true;
+            for (int[] obs : obstracles) {
+                if (startRow + 1 == obs[0] && startCol == obs[1]) {
+                    allowed = false;
+                    break;
+                }
+            }
+            if (allowed) {
+                result.addAll(ways(maze, startRow + 1, startCol, endRow, endCol, currentWay + "D", obstracles));
+            }
         }
 //        Right
         if (startRow < maze.length) {
-            result.addAll(ways(maze, startRow, startCol + 1, endRow, endCol, currentWay + "R"));
+            boolean allowed = true;
+            for (int[] obs : obstracles) {
+                if (startRow == obs[0] && startCol + 1 == obs[1]) {
+                    allowed = false;
+                    break;
+                }
+            }
+            if (allowed) {
+                result.addAll(ways(maze, startRow, startCol + 1, endRow, endCol, currentWay + "R", obstracles));
+            }
         }
         return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(ways(new int[3][3], 0, 0, 2, 2));
+        int[][] obstracles = {{1, 1},{0,2}};
+        System.out.println(ways(new int[3][3], 0, 0, 2, 2, obstracles));
     }
 }
