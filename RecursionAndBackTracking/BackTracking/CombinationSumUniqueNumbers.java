47a6ac21a8a64d55226684637efa6ea02e61229d
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-07-11 18:38:55 +0530

    Recursion Combination Sum Unique Numbers

--- RecursionAndBackTracking/BackTracking/CombinationSumUniqueNumbers.java
+++ RecursionAndBackTracking/BackTracking/CombinationSumUniqueNumbers.java
@@ -1,13 +1,11 @@
 package RecursionAndBackTracking.BackTracking;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class CombinationSumUniqueNumbers {
     static List<List<Integer>> combinationSumUniqueNumbers(int[] array, int target) {
         return combinationSumUniqueNumbers(array, target, 0, new ArrayList<>(), new HashSet<>());
+//        return combinationSumUniqueNumbers(array, target, 0, new ArrayList<>());
     }
 
     static List<List<Integer>> combinationSumUniqueNumbers(int[] array, int target, int index, List<Integer> currentList, Set<Integer> usedIndex) {
@@ -18,18 +16,36 @@ public class CombinationSumUniqueNumbers {
         }
         for (int i = index; i < array.length; i++) {
             if (!usedIndex.contains(i) && target >= array[i]) {
-                usedIndex.add(array[i]);
+                usedIndex.add(i);
                 currentList.add(array[i]);
                 result.addAll(combinationSumUniqueNumbers(array, target - array[i], i, currentList, usedIndex));
                 currentList.removeLast();
-                usedIndex.remove(array[i]);
+                usedIndex.remove(i);
             }
         }
         return result;
     }
 
+    static List<List<Integer>> combinationSumUniqueNumbers(int[] array, int target, int index, List<Integer> currentList) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (0 == target) {
+            Collections.sort(currentList);
+            result.add(new ArrayList<>(currentList));
+            return result;
+        }
+        if(index==array.length){
+            return result;
+        }
+        currentList.add(array[index]);
+        result.addAll(combinationSumUniqueNumbers(array, target - array[index], index + 1, currentList));
+        currentList.removeLast();
+        result.addAll(combinationSumUniqueNumbers(array, target, index + 1, currentList));
+
+        return result;
+    }
+
     public static void main(String[] args) {
-        int[] array = {10, 1, 2, 7, 6,  5};
+        int[] array = {10, 1, 2, 7, 6, 1, 5};
         List<List<Integer>> lists = combinationSumUniqueNumbers(array, 8);
         System.out.println(lists);
     }
