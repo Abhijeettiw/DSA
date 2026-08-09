fd75af8ad1f69932e8c87cf9206d9ed94a8162c0
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-08-09 16:02:14 +0530

    Tree depth first search - Flatten Tree Node Into List

--- Tree/DepthFirstSearch/FlattenTreeNodeIntoList.java
+++ Tree/DepthFirstSearch/FlattenTreeNodeIntoList.java
@@ -16,11 +16,32 @@ public class FlattenTreeNodeIntoList {
         result.addAll(flattenTreeNodeIntoList(node.getRight()));
         return result;
     }
+    static BinaryTree.TreeNode getRightMostNode(BinaryTree.TreeNode node){
+        if(node.getRight()==null){
+            return node;
+        }
+        return getRightMostNode(node.getRight());
+    }
+    static BinaryTree.TreeNode flattenTreeNodeIntoListLeetCode(BinaryTree.TreeNode node) {
+        if (node == null) {
+            return node;
+        }
+        if(node.getLeft() != null){
+            BinaryTree.TreeNode rightMostNode = getRightMostNode(node.getLeft());
+            rightMostNode.setRight(node.getRight());
+            node.setRight(node.getLeft());
+            node.setLeft(null);
+        }
+        flattenTreeNodeIntoListLeetCode(node.getRight());
+        return node;
+    }
 
     public static void main(String[] args) {
         BinaryTree tree = new BinaryTree();
         tree.populate();
         List<Integer> list = flattenTreeNodeIntoList(tree.getRoot());
         System.out.println(list);
+        BinaryTree.TreeNode root = flattenTreeNodeIntoListLeetCode(tree.getRoot());
+        System.out.println(root);
     }
 }
