66ad84336f628405539f7414c3a777a09fb74d45
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-08-09 21:16:10 +0530

    Tree depth first search - Validate A Binary Search Tree

--- Tree/DepthFirstSearch/ValidateABinarySearchTree.java
+++ Tree/DepthFirstSearch/ValidateABinarySearchTree.java
@@ -3,7 +3,7 @@ package Tree.DepthFirstSearch;
 import Tree.BinaryTree;
 
 public class ValidateABinarySearchTree {
-    static boolean validateABinarySearchTree(BinaryTree.TreeNode node) {
+    static boolean validateABinarySearchTree1(BinaryTree.TreeNode node) {
         if (node == null) {
             return true;
         }
@@ -15,18 +15,37 @@ public class ValidateABinarySearchTree {
         if (right != null && node.getData() > right.getData()) {
             return false;
         }
-        boolean a = validateABinarySearchTree(node.getLeft());
-        boolean b = validateABinarySearchTree(node.getRight());
-        if (!(a && b)) {
+        boolean a = validateABinarySearchTree1(node.getLeft());
+        boolean b = validateABinarySearchTree1(node.getRight());
+        return a && b;
+    }
+
+    static boolean validateABinarySearchTree2(BinaryTree.TreeNode node) {
+        if (node == null) {
+            return true;
+        }
+        return validateABinarySearchTree2(node, null, null);
+    }
+
+    static boolean validateABinarySearchTree2(BinaryTree.TreeNode node, Integer low, Integer high) {
+        if (node == null) {
+            return true;
+        }
+        if (low != null && low >= node.getData()) {
             return false;
         }
-        return true;
+        if (high != null && high <= node.getData()) {
+            return false;
+        }
+        boolean l = validateABinarySearchTree2(node.getLeft(), low, node.getData());
+        boolean r = validateABinarySearchTree2(node.getRight(), node.getData(), high);
+        return l && r;
     }
 
     public static void main(String[] args) {
         BinaryTree tree = new BinaryTree();
         tree.populate();
-        System.out.println(validateABinarySearchTree(tree.getRoot()));
+        System.out.println(validateABinarySearchTree1(tree.getRoot()));
 
     }
 }
