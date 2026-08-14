bb1cc91f240266e4fdb15a0cf863933e228288c2
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-08-14 22:38:45 +0530

    Heaps - offer & poll method of max heap

--- Heaps/HeapTest.java
+++ Heaps/HeapTest.java
@@ -19,5 +19,23 @@ public class HeapTest {
         Integer poll2 = minHeap.poll();
         System.out.println(poll2);
         System.out.println(minHeap.innerList);
+
+        Heaps maxHeap = new MaxHeap();
+        maxHeap.offer(1);
+        maxHeap.offer(4);
+        maxHeap.offer(3);
+        maxHeap.offer(2);
+        maxHeap.offer(8);
+        maxHeap.offer(7);
+        maxHeap.offer(5);
+        maxHeap.offer(-1);
+        maxHeap.offer(6);
+        System.out.println(maxHeap.innerList);
+        Integer poll3 = maxHeap.poll();
+        System.out.println(poll3);
+        System.out.println(maxHeap.innerList);
+        Integer poll4 = maxHeap.poll();
+        System.out.println(poll4);
+        System.out.println(maxHeap.innerList);
     }
 }
--- Heaps/MaxHeap.java
+++ Heaps/MaxHeap.java
@@ -2,16 +2,68 @@ package Heaps;
 
 import java.util.ArrayList;
 
-public class MaxHeap extends Heaps{
-    public MaxHeap(){
+public class MaxHeap extends Heaps {
+    public MaxHeap() {
         this.innerList = new ArrayList<>();
     }
+
     @Override
     void offer(Integer val) {
-
+        int currentIndex = innerList.size();
+        innerList.add(val);
+        if (0 == currentIndex) {
+            return;
+        } else if (1 == currentIndex) {
+            offer(val, 0, currentIndex);
+            return;
+        } else if (2 == currentIndex) {
+            offer(val, 0, currentIndex);
+            return;
+        }
+        int parentIndex = currentIndex % 2 == 0 ? (currentIndex - 1) / 2 : currentIndex / 2;
+        offer(val, parentIndex, currentIndex);
     }
+
+    void offer(Integer val, int parentIndex, int childIndex) {
+        Integer parentVal = innerList.get(parentIndex);
+        if (parentVal < val) {
+            innerList.set(parentIndex, val);
+            innerList.set(childIndex, parentVal);
+            int newParentIndex = parentIndex % 2 == 0 ? (parentIndex - 1) / 2 : parentIndex / 2;
+            offer(val, newParentIndex, parentIndex);
+        }
+    }
+
     @Override
     Integer poll() {
-        return null;
+        Integer removedElement = innerList.removeFirst();
+        Integer lastElement = innerList.removeLast();
+        innerList.addFirst(lastElement);
+        poll(0);
+        return removedElement;
+    }
+
+    void poll(int index) {
+        if (!isValidIndex(index)) {
+            return;
+        }
+        Integer leftIndex = (index * 2) + 1;
+        if (isValidIndex(leftIndex)) {
+            Integer value = innerList.get(index);
+            if (value < innerList.get(leftIndex)) {
+                innerList.set(index, innerList.get(leftIndex));
+                innerList.set(leftIndex, value);
+                poll(leftIndex);
+            }
+            int rightIndex = leftIndex + 1;
+            if (isValidIndex(rightIndex)) {
+                value = innerList.get(index);
+                if (value < innerList.get(rightIndex)) {
+                    innerList.set(index, innerList.get(rightIndex));
+                    innerList.set(rightIndex, value);
+                    poll(rightIndex);
+                }
+            }
+        }
     }
 }
