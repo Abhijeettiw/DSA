35486c6025c0121199138de7e73adad9cb911c78
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-08-24 21:31:19 +0530

    Depth first search - Lowest Common Ancestor

--- Tree/DepthFirstSearch/LowestCommonAncestor.java
+++ Tree/DepthFirstSearch/LowestCommonAncestor.java
@@ -33,6 +33,23 @@ public class LowestCommonAncestor {
         lowestCommonAncestor(treeNode.getRight(), node1, node2);
     }
 
+//    LeetCode
+//    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//        if(root==null){
+//            return null;
+//        }
+//        if(root==p || root==q){
+//            return root;
+//        }
+//        TreeNode left = lowestCommonAncestor(root.left,p,q);
+//        TreeNode right = lowestCommonAncestor(root.right,p,q);
+//        if(left != null && right != null){
+//            return root;
+//        }
+//
+//        return left == null?right :left;
+//    }
+
     public static void main(String[] args) {
         BinaryTree tree = new BinaryTree();
         tree.populate();
