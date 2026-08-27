68f90ff73ebcfbe5fb1959b539c56d10f329b8e6
Abhijeet <abhijeet1992tiwari@gmail.com>
2026-08-27 21:37:10 +0530

    Depth first search - Kth Smallest From BST

--- Tree/DepthFirstSearch/GetKthSmallestFromBST.java
+++ Tree/DepthFirstSearch/GetKthSmallestFromBST.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 public class GetKthSmallestFromBST {
 
+    static int cnt = 0;
+
     static Integer getKthSmallestFromBST(BinarySearchTree.TreeNode node, int k) {
         List<Integer> allElementBst = getKthSmallestFromBST(node);
         if (k > allElementBst.size()) {
@@ -26,9 +28,35 @@ public class GetKthSmallestFromBST {
         return res;
     }
 
+//    From course
+    static Integer kthSmallestFromBST(BinarySearchTree.TreeNode node, int k) {
+        BinarySearchTree.TreeNode treeNode = kthSmallestFromBSTHelper(node, k);
+        if (treeNode != null) {
+            return treeNode.getData();
+        }
+        return null;
+    }
+
+    static BinarySearchTree.TreeNode kthSmallestFromBSTHelper(BinarySearchTree.TreeNode node, int k) {
+        if (node == null) {
+            return null;
+        }
+        BinarySearchTree.TreeNode left = kthSmallestFromBSTHelper(node.getLeft(), k);
+        if (left != null) {
+            return left;
+        }
+        cnt++;
+        if (k == cnt) {
+            return node;
+        }
+        return kthSmallestFromBSTHelper(node.getRight(), k);
+
+    }
+
     public static void main(String[] args) {
         BinarySearchTree bst = new BinarySearchTree();
         bst.insert();
         System.out.println(getKthSmallestFromBST(bst.getRoot(), 2));
+        System.out.println(kthSmallestFromBST(bst.getRoot(), 2));
     }
 }
